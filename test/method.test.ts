3948152393eea4e1eea8c0f944ff5303f52183ec
ren-wei <demonre@foxmail.com>
2022-04-01 10:57:33 +0800

    test: add method test suite

---
+++ test/method.test.ts
@@ -0,0 +1,28 @@
+import { mount, createLocalVue } from '@vue/test-utils';
+import { describe, it, expect } from '@jest/globals';
+// Why is VueRouter undefined?
+// import VueRouter from 'vue-router';
+const VueRouter = require('vue-router');
+import Anchor from '../src/anchor';
+
+const localVue = createLocalVue();
+localVue.use(VueRouter);
+
+const app = {
+    template: '<div></div>',
+};
+const router = new VueRouter({ routes: [{ path: '/', component: app }] });
+const wrapper = mount(app, {
+    localVue,
+    router,
+});
+const anchor = new Anchor(wrapper.vm);
+
+describe('method', () => {
+    it('unpack', async() => {
+        expect(anchor.unpack('sa')).toBe('a');
+        expect(anchor.unpack(['t'])).toBeTruthy();
+        expect(anchor.unpack(undefined)).toBeUndefined();
+        expect(() => anchor.unpack([null])).toThrow();
+    });
+});
